dae0f1cec2886f7e111f5099ef1583c1bf53b186
shubhT25 <shubhamtayade11235@gmail.com>
2024-01-23 22:56:00 +0530

    Minor Changes

--- src/Algoritms/Ceiling.java
+++ src/Algoritms/Ceiling.java
@@ -2,11 +2,12 @@ package Algoritms;
 
 public class Ceiling {
   public static void main(String[] args) {
-    int [] arr = {2,3,5,8,9,16,18};
-    int target = 10;
+    int [] arr = {2,3,5,7,8,9,12,14};
+    int target = 14;
     int low=0, high= arr.length-1;
     int mid = (low+high)/2;
     while(low<high) {
+
       if(arr[mid] == target) {
         System.out.println("Ceiling:"+ arr[mid]);
         break;
@@ -17,7 +18,10 @@ public class Ceiling {
         if(arr[mid+1] > target) {
           System.out.println("Ceiling:"+ arr[mid]);
           break;
-        } else {
+        } else if (arr[mid+1] == target) {
+          System.out.println("Ceiling:"+ arr[mid+1]);
+          break;
+        }else {
           low=mid;
           mid=(low+high)/2;
         }
